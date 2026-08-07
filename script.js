6b191a878774792aac3e0e8c98158563d7662410
Anamol Raj Singh <anamolrajsingh@gmail.com>
2026-08-07 03:30:25 +0000

    Diagnostic: set title to JS:LOADED at IIFE start to verify execution

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 
 (function () {
     'use strict';
+    document.title = 'JS:LOADED';
+    var navClockEl = document.getElementById('navClock');
+    if (navClockEl) navClockEl.textContent = 'JS:OK';
 
     // Helper: safe getElementById
     function $(id) { return document.getElementById(id); }
