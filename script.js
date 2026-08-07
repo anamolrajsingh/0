0f3ba346dfeb4e6797550b9da681626903ce26d4
Anamol Raj Singh <anamolrajsingh@users.noreply.github.com>
2026-08-07 05:12:24 +0000

    Fix: update YouTube embed to a working video ID

--- script.js
+++ script.js
@@ -236,7 +236,7 @@
     // Embed URLs (loaded only when drawer opens)
     var vibesUrls = {
         spotify: 'https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO?utm_source=generator&theme=0',
-        ytmusic: 'https://www.youtube.com/embed/jfpeBajrZyg'
+        ytmusic: 'https://www.youtube.com/embed/DWcJFNfaw9c'
     };
 
     // Load an iframe into a panel (only once)
